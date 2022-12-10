931a9396efb5ff0639b410383ad5090da17834fc
scoutboy05 <69093724+gittyboy-cell@users.noreply.github.com>
2022-12-10 16:24:38 +0700

    update kalilinuxify.sh

--- kalilinuxify.sh
+++ kalilinuxify.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 #script to change any debian based linux distros to kali linux
 
-#checking root
+# checking root
 
 if [ "$EUID" -ne 0 ]
   then echo "Please run as root"
   exit
 fi
 
-#adding repo
+# adding repo
 
 #echo deb http://http.kali.org/kali kali-rolling main contrib non-free >> /etc/apt/sources.list.d/kalilinux.list
 
-#installing additional metapackages
 
-echo what meta
+# installing additional metapackages
+
+echo what metapackages to install?
 read metapackage
-echo ok,installing the following meta packages = $metapackage 
+echo ok,installing the following metapackages = $metapackage 
 apt install
